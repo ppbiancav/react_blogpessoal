81ac6c2691b810e6e0cf406ad7a571d8dae61f23
Bianca <biancappv@gmail.com>
2024-05-27 10:49:01 -0300

    feat: implementando o CRUD de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
 import Home from './paginas/home/Home';
-import Cadastro from './paginas/cadastro/Cadastro';
-import Footer from './componentes/footer/Footer';
-import Navbar from './componentes/navbar/Navbar';
 import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
+import Navbar from './componentes/navbar/Navbar';
+import Footer from './componentes/footer/Footer';
+import DeletarTema from './componentes/tema/deletartema/DeletarTema';
+import FormTema from './componentes/tema/formtema/FormTema';
+import ListaTemas from './componentes/tema/listatemas/ListaTemas';
+
+
 
 
 function App() {
   return (
-    <>
-    <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
-    </>
-  );
+      <>
+          {/* 
+              Envolvemos todos os Componentes inseridos no Componente App, 
+              com o Componente AuthProvider. 
+              Desta forma, todos os Componentes dentro dele, terão acesso 
+              aos Estados e Funções guardados na context.**
+          */}
+          <AuthProvider>
+              <BrowserRouter>
+                  <Navbar />
+                  <div className='min-h-[80vh]'>
+                      <Routes>
+                          <Route path="/" element={<Login />} />
+                          <Route path="/home" element={<Home />} />
+                          <Route path="/cadastro" element={<Cadastro />} />
+                          <Route path="/login" element={<Login />} />
+                          <Route path="/temas" element={<ListaTemas />} />
+                          <Route path="/cadastrartema" element={<FormTema />} />
+                          <Route path="/editartema/:id" element={<FormTema />} />
+                          <Route path="/deletartema/:id" element={<DeletarTema />} />
+                      </Routes>
+                  </div>
+                  <Footer />
+              </BrowserRouter>
+          </AuthProvider>
+      </>
+  )
 }
-export default App;
+
+export default App
 
 //organizar os componentes.
